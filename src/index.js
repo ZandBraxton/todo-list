7346fbd78d40c077902eaa35c91c944dd06b5239
ZandBraxton <Zand.Braxton@gmail.com>
2021-08-17 12:48:32 -0500

    Added tab switching function and started basic sidebar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './style.css';
 
 
+const domWriter = (() => {
+   
+
+
+})();
+
+
+const tabSwitcher = (() => {
+    const sideBar = document.querySelectorAll('.sidebar-items')
+    sideBar.forEach(item => {
+        item.addEventListener('click', () => {
+            let id = item.id
+            switchTab(id)
+        })
+    })
+
+    function switchTab(id) {
+        const activeBtn = document.querySelector('.active')
+        if (activeBtn) activeBtn.classList.remove('active')
+
+        const tab = document.getElementById(id)
+        tab.classList.add('active')
+    }
+
+})();
