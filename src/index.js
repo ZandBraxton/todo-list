6f691a6b5b278db69715d45ef99bb26d623109ea
ZandBraxton <Zand.Braxton@gmail.com>
2021-08-18 14:35:24 -0500

    Added form submit function and ran it through a constructor for tasks

--- src/index.js
+++ src/index.js
@@ -5,15 +5,11 @@ import createUpcoming from './sidebar-modules/upcoming'
 import format from 'date-fns/format';
 
 
-const taskFactory = (title, desc, dueDate, priority) => {
-    return {title, desc, dueDate, priority}
+class taskFactory {
+    constructor(title, desc, dueDate, priority) {
+        return {title, desc, dueDate, priority}
+    }
 }
-
-
-
-
-
-
 
 
 const domWriter = (() => {
@@ -43,27 +39,55 @@ const domWriter = (() => {
     }
 
     function createTaskForm() {
-        //need more parameters and css
-        //look up date-fns docs for data info
         let checkForm = document.querySelector('form')
         if (!!checkForm) {
             return
         }
         const form = document.createElement('form')
-        createInput("Title", form)
-        createInput("Description", form)
+        createInput("Title", 'text', form)
+        createInput("Description", 'text', form)
+        createInput("Date", 'date', form)
+        createPriority(form)
+        createInput("Submit", 'submit', form)
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let newTask = new taskFactory(
+                form.elements[0].value,
+                form.elements[1].value, 
+                form.elements[2].value, 
+                form.elements[3].value)
+                console.log(newTask)
+                main.removeChild(form)
+        })
         main.appendChild(form)
         
     }
 
-    function createInput(name, element) {
+    function createInput(name, type, element) {
         const input = document.createElement("input")
-        input.required = true
-        input.setAttribute('type', 'text')
-        input.setAttribute('name', name)
-        input.setAttribute('placeholder', name)
+        // input.required = true
+        input.type = type
+        input.name = name
+        input.placeholder = name
+        if (name === "Submit") {
+            input.value = "Add Task"
+        }
         element.appendChild(input)
     }
+
+    function createPriority(element) {
+        const values = [1, 2, 3, 4, 5]
+        const dropdown = document.createElement('select')
+        dropdown.name = "Priority"
+        for (const val of values) {
+            const option = document.createElement('option')
+            option.value = val
+            option.text = val
+            dropdown.appendChild(option)
+        }
+        element.appendChild(dropdown)
+    }
+
 
     function getDate() {
         const time = document.querySelector('.time')
